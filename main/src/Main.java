8b1f25b501aa895b9d51a3eb6d95fb57cc458dc5
spiderasasen <spiderasasen@gmail.com>
2025-12-10 12:04:09 -0500

    adding an image with a ur

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
 void main(String[] args) {
     show(new JFrame());
@@ -8,14 +12,32 @@ void main(String[] args) {
 public static void show(JFrame parent){
     System.out.println("Creates the page");
     JFrame frame = new JFrame("Spinning GIF");
-    frame.setSize(400, 500);
+    frame.setSize(900, 1000);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-    //creates a button
-    JButton button = new JButton("Click");
-    frame.add(button);
+    //creates a image
+    String imageURL = "https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_640.png";
 
+    //loads the image
+    BufferedImage image = null;
+    try {
+        URL url = new URL(imageURL);
+        image = ImageIO.read(url); //reads the url
+    } catch (IOException e) { //if no image can be loaded then it will have an error
+        e.printStackTrace();
+        System.err.println("Error loading image from URL: " + imageURL);
+    }
+
+    //will print the image into the frame
+    if (image != null) {
+        ImageIcon imageIcon = new ImageIcon(image);
+        JLabel label = new JLabel(imageIcon);
+        frame.add(label);
+    } else { //image could not be loaded
+        JLabel errorLabel = new JLabel("Could not load image from URL.");
+        frame.add(errorLabel);
+    }
 
     //ALWAYS HAVE THIS AT THE END
     frame.setVisible(true);
