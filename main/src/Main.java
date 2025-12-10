1f9468067ef884a763c34e689072de62d8c9d22f
spiderasasen <spiderasasen@gmail.com>
2025-12-10 11:23:35 -0500

    makes a simple page that will just add a button

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,3 +1,13 @@
+import javax.swing.*;
+
 void main(String[] args) {
-    System.out.println("Hello World!!");
+    JFrame frame = new JFrame("Spinning GIF");
+    frame.setSize(400, 500);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //creates a button
+    JButton button = new JButton("Click");
+    frame.add(button);
+
+    frame.setVisible(true);
 }
